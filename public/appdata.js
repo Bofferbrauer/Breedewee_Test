fa741c86dc1539136cea1aad06e81985bc54d6a5
Steve Weidig <75318450+Bofferbrauer@users.noreply.github.com>
2021-05-11 14:57:26 +0200

    ionic

--- public/appdata.js
+++ public/appdata.js
@@ -1,3 +1,14 @@
 console.log(firebase);
 
 const auth = firebase.auth();
+
+const SignIn = document.querySelector("SignIn");
+const SignOut = document.querySelector("SignOut");
+const whenSignedIn = document.querySelector("whenSignedIn");
+const whenSignedout = document.querySelector("whenSignedOut");
+
+const provider = new firebase.auth.GoogleAuthProvider();
+
+document.querySelector("h1").style.textAlign = "center";
+document.querySelector("h1").style.fontSize = "50px";
+document.querySelector("h1").style.textDecoration = "underline";
---
+++ table-test/src/app/home/page.js
@@ -0,0 +1,10 @@
+const editData = document.querySelector(".EditEntry");
+const deleteData = document.querySelector(".DeleteEntry");
+const dataItems = document.querySelector(".DataList");
+
+deleteData.addEventListener("click", () =>{
+  console.log("Test successful");
+});
+editData.addEventListener("click", () =>{
+  console.log("Test successful");
+});
